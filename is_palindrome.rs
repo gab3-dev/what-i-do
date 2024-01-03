3b97a6096590e5c70a37d42a384fd3c4ca7feef4
gab3-dev <gabe.dev@icloud.com>
2024-01-02 23:38:09 -0300

    Fix algorithm

--- is_palindrome.rs
+++ is_palindrome.rs
@@ -1,6 +1,6 @@
 impl Solution {
     pub fn is_palindrome(s: String) -> bool {
-        let sl= s.to_lowercase();
+        let s1 = s.to_lowercase();
         let iter = s1.chars().filter(
             |c| c.is_ascii_alphanumeric()
         );
